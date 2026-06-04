501daae1f1a2a73754bcd6b420313d0e5a43c15f
Utkarsh Shukla <utkarshukla2205@gmail.com>
2026-06-04 23:08:13 +0530

    Added LinkedLists Folder

---
+++ LinkedLists/swap-nodes-in-pair.py
@@ -0,0 +1,19 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        dummy = ListNode()
+        dummy.next = head
+        prev = dummy
+        while prev.next and prev.next.next:
+            first = prev.next
+            second = first.next
+
+            first.next = second.next
+            second.next = first
+            prev.next = second
+
+            prev = first
+        return dummy.next
